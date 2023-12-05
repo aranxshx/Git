3e2fadd7d1bdd102b866f81d29345982624690ea
aranxshx <andreweroyla123@gmail.com>
2023-12-06 00:27:22 +0800

    second commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
 
 using namespace std;
 
+void slowPrint(const string& text, int delayTime)
+{
+    for (size_t i = 0; i != text.length(); i++)
+    {
+        cout << text[i];
+        Sleep(delayTime);
+    }
+}
+
+void box(const string& text)
+{
+    const int boxW = text.length() + 4;
+
+    cout << string(boxW, '-') << endl;
+    cout << "| " << text << " |" << endl;
+    cout << string(boxW, '-') << endl;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    Sleep(1000);
+
+    
 }
